18458d3975c62b09da1cbd020635559343f05d8c
Juan Camilo Espinosa Briceño <juancamiloespinosab@gmail.com>
2020-09-09 17:15:02 -0500

    Se crea componente para visualizar el poster de una serie

--- src/app/detail/poster/poster.component.ts
+++ src/app/detail/poster/poster.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as interfaces from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-poster',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PosterComponent implements OnInit {
 
+  @Input() detail: interfaces.Detail;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getImageBackground() {
+    return `url('https://image.tmdb.org/t/p/w500/${this.detail.imgPosterPath}')`;
+
   }
 
 }
